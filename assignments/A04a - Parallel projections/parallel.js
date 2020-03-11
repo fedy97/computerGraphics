769ec105545c418c92f50fafbe8cf7cfd8de0999
fedy97 <frc.morreale@gmail.com>
2020-03-11 16:26:16 +0100

    4a assignment done

--- assignments/A04a - Parallel projections/parallel.js
+++ assignments/A04a - Parallel projections/parallel.js
@@ -1,12 +1,18 @@
 function parallel() {
-	// Build a parallel projection matrix, for a 16/9 viewport, with halfwidt w=40, near plane n=1, and far plane f=101.
-	var out = [1.0,		0.0,		0.0,		0.0,
+	// Build a parallel projection matrix, for a 16/9 viewport, with halfwidth w=40, near plane n=1, and far plane f=101.
+	let out = [1.0,		0.0,		0.0,		0.0,
 			   0.0,		1.0,		0.0,		0.0,
 			   0.0,		0.0,		1.0,		0.0,
 			   0.0,		0.0,		0.0,		1.0];
-
-
-
+	out = parallelProjection(out,16/9, 40, 1, 101);
 	return out;
+}
+
+function parallelProjection(matrix, ratio, w, near ,far) {
+	matrix[0] = 1/w;
+	matrix[5] = ratio/w;
+	matrix[10] = -2/(far-near);
+	matrix[11] = -(far+near)/(far-near);
+	return matrix;
 }
 
