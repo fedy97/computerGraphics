3ff44b0c290c467b6a931514c4e66b63b5171136
fedy97 <frc.morreale@gmail.com>
2020-03-18 16:59:57 +0100

    5 assignment done

--- assignments/A05 - Perspective projection and View Matrix/perspective.js
+++ assignments/A05 - Perspective projection and View Matrix/perspective.js
@@ -1,10 +1,19 @@
 function perspective(w, h, fov) {
-	// Build a perspective projection matrix, for a viewport whose size is determined by parameters w (width) and h (height), and whose fov-y is passed in parameter fov. Near plane is n=0.1, and far plane f=100.
-	var out = [0.01,		0.0,		0.0,		0.0,
-			   0.0,		0.01,		0.0,		0.0,
-			   0.0,		0.0,		0.01,		0.0,
-			   0.0,		0.0,		0.0,		1.0];
+    // Build a perspective projection matrix,
+    // for a viewport whose size is determined by parameters w (width) and h (height),
+    // and whose fov-y is passed in parameter fov. Near plane is n=0.1, and far plane f=100.
+    return perspectiveMatrix(w, h, fov, 0.1, 100);
+}
 
-	return out;
+function perspectiveMatrix(w, h, fov, near, far) {
+    var a = w / h;
+    var matrix = utils.identityMatrix();
+    matrix[0] = 1 / (a * Math.tan(utils.degToRad(fov) / 2));
+    matrix[10] = (far + near) / (near - far);
+    matrix[5] = 1 / (Math.tan(utils.degToRad(fov) / 2));
+    matrix[11] = (2 * far * near) / (near - far);
+    matrix[14] = -1.0;
+    matrix[15] = 0.0;
+    return matrix;
 }
 
--- assignments/A05 - Perspective projection and View Matrix/view.js
+++ assignments/A05 - Perspective projection and View Matrix/view.js
@@ -1,13 +1,14 @@
-
 function view(cx, cy, cz, alpha, beta, rho) {
-	// Create a view matrix for a camera in position cx, cy and cz, looking in the direction specified by
-	// alpha, beta and rho, as outlined in the course slides.
-	var out =  [1.0,	0.0,		0.0,		0.0,
-			   0.0,		1.0,		0.0,		0.0,
-			   0.0,		0.0,		1.0,	    -50.0,
-			   0.0,		0.0,		0.0,		1.0];
-			   
+    // Create a view matrix for a camera in position cx, cy and cz, looking in the direction specified by
+    // alpha, beta and rho, as outlined in the course slides.
 
-	return out;
+    var invRotZ = utils.MakeRotateZMatrix(-rho);
+    var invRotX = utils.MakeRotateXMatrix(-beta);
+    var invRotY = utils.MakeRotateYMatrix(-alpha);
+    var invTras = utils.MakeTranslateMatrix(-cx, -cy, -cz);
+    var out1 = utils.multiplyMatrices(invRotZ, invRotX);
+    var out2 = utils.multiplyMatrices(out1, invRotY);
+    var out = utils.multiplyMatrices(out2, invTras);
+    return out;
 }
 
