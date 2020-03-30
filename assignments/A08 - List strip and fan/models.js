02b986a849de1cc2767344ed598f2e02b23dc97e
fedy97 <frc.morreale@gmail.com>
2020-03-30 15:36:26 +0200

    8 assignment done

--- assignments/A08 - List strip and fan/models.js
+++ assignments/A08 - List strip and fan/models.js
@@ -1,24 +1,25 @@
 function buildGeometry() {
-	var i;
-	
-	// Draws a cube (replace the vertices)
-	var vert1 = [[-1.0,-1.0,-1.0], [1.0,-1.0,-1.0], [0.0,1.0,0.0], [-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [0.0, 0.0, 0.0]];
+    var i;
 
-	addMesh(vert1, "L", [1.0, 0.0, 0.0]);
+    // Draws a cube (replace the vertices)
+    //if Y stays at 0, the figure is hidden because has no height
+    var vert1 = [[0.0, 0.0, 1.0], [0.0, 0.0, 0.0], [1.0, 0.0, 1.0], [1.0, 0.0, 0.0], [1, 1, 1], [1, 1, 0],
+        [0, 1, 0], [1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1], [0, 1, 0]];
 
-
-	// Draws a flat L-shaped pattern (replace the vertices)
-	var vert2 = [[-1.0,-1.0,-1.0], [1.0,-1.0,-1.0], [1.0, 1.0,-1.0], [-1.0, 1.0,-1.0], [-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [1.0, 1.0, 1.0], [-1.0, 1.0, 1.0]];
-
-	addMesh(vert2, "S", [0.0, 0.0, 1.0]);
+    addMesh(vert1, "S", [1.0, 0.0, 0.0]);
 
 
-	// Draws a flat hexagon (replace the vertices)
-	var vert3 = [[-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [1.0,1.0, 1.0], [-1.0,1.0, 1.0], [0.0,1.5, 1.0],
-				 [-1.0,-1.0,-1.0], [1.0,-1.0,-1.0], [1.0,1.0,-1.0], [-1.0,1.0,-1.0], [0.0,1.5,-1.0]
-	];
+    // Draws a flat L-shaped pattern (replace the vertices)
+    var vert2 = [[2, 0, 0], [0, 0, 0], [2, 1, 0], [0, 1, 0], [1, 1, 0], [0, 3, 0], [1, 3, 0]];
 
-	addMesh(vert3, "F", [0.0, 1.0, 0.0]);
-	
+    addMesh(vert2, "S", [0.0, 0.0, 1.0]);
+
+
+    // Draws a flat hexagon (replace the vertices)
+    var vert3 = [[0, 0, 0], [1, 0, 0], [0.5, -0.866, 0], [-0.5,-0.866,0],[-1,0,0],[-0.5,0.866,0],[0.5,0.866,0],[1,0,0]
+    ];
+
+    addMesh(vert3, "F", [0.0, 1.0, 0.0]);
+
 }
 
