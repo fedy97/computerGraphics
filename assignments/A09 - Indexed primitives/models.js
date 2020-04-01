1d370677eedf6681f45e9c0e43a75e32f62d77e2
fedy97 <frc.morreale@gmail.com>
2020-04-01 22:59:34 +0200

    9 assignment done

--- assignments/A09 - Indexed primitives/models.js
+++ assignments/A09 - Indexed primitives/models.js
@@ -1,101 +1,171 @@
 function buildGeometry() {
-	var i;
-	
-	// Draws a Cone --- Already done, just for inspiration
-	///// Creates vertices
-	var vert1 = [[0.0, 1.0, 0.0]];
-	for(i = 0; i < 36; i++) {
-		vert1[i+1] = [Math.sin(i*10.0/180.0*Math.PI), -1.0, Math.cos(i*10.0/180.0*Math.PI)];
-	}
-	vert1[37] = [0.0, -1.0, 0.0]
-	////// Creates indices
-	var ind1 = [];
-	//////// Upper part
-	j = 0;
-	for(i = 0; i < 36; i++) {
-		ind1[j++] = 0;
-		ind1[j++] = i + 1;
-		ind1[j++] = (i + 1) % 36 + 1;
-	}
-	//////// Lower part
-	for(i = 0; i < 36; i++) {
-		ind1[j++] = 37;
-		ind1[j++] = (i + 1) % 36 + 1;
-		ind1[j++] = i + 1;
-	}
-	
-	var color1 = [1.0, 0.0, 0.0];
-	addMesh(vert1, ind1, color1);
+    var i;
+
+    // Draws a Cone --- Already done, just for inspiration
+    ///// Creates vertices
+    var vert1 = [[0.0, 1.0, 0.0]];
+    for (i = 0; i < 36; i++) {
+        vert1[i + 1] = [Math.sin(i * 10.0 / 180.0 * Math.PI), -1.0, Math.cos(i * 10.0 / 180.0 * Math.PI)];
+    }
+    vert1[37] = [0.0, -1.0, 0.0];
+    ////// Creates indices
+    var ind1 = [];
+    //////// Upper part
+    let j = 0;
+    for (i = 0; i < 36; i++) {
+        ind1[j++] = 0;
+        ind1[j++] = i + 1;
+        ind1[j++] = (i + 1) % 36 + 1;
+    }
+    //////// Lower part
+    for (i = 0; i < 36; i++) {
+        ind1[j++] = 37;
+        ind1[j++] = (i + 1) % 36 + 1;
+        ind1[j++] = i + 1;
+    }
+
+    var color1 = [1.0, 0.0, 0.0];
+    addMesh(vert1, ind1, color1);
 
 
+    // Draws a Cylinder -- To do for the assignment.
+    var vert2 = [[0.0, -1.0, 0.0]];
+    //base bottom vertexes (from 0 to 37)
+    for (i = 0; i < 36; i++) {
+        vert2[i + 1] = [Math.sin(i * 10.0 / 180.0 * Math.PI), -1.0, Math.cos(i * 10.0 / 180.0 * Math.PI)];
+    }
+    //bottom center
+    vert2[37] = [0.0, -1.0, 0.0];
+    //base top vertexes (from 38 to 75)
+    vert2[38] = [0.0, 1.0, 0.0];
+    for (i = 38; i < 74; i++) {
+        vert2[i + 1] = [Math.sin((i - 38) * 10.0 / 180.0 * Math.PI), 1.0, Math.cos((i - 38) * 10.0 / 180.0 * Math.PI)];
+    }
+    //top center
+    vert2[75] = [0.0, 1.0, 0.0];
+
+    let ind2 = [];
+    j = 0;
+
+    //////// Upper part indexes
+    for (i = 0; i < 36; i++) {
+        ind2[j++] = 38;
+        ind2[j++] = i + 1 + 38;
+        ind2[j++] = ((i + 1) % 36) + 1 + 38;
+    }
+    //////// Lower part indexes
+    for (i = 0; i < 36; i++) {
+        ind2[j++] = 0;
+        ind2[j++] = (i + 1) % 36 + 1;
+        ind2[j++] = i + 1;
+    }
+
+    //intorno indexes ( need vertexes from 1 to 36 and from 39 to 74 )
+    for (i = 0; i < 36; i++) {
+        ind2[j++] = i + 1;
+        ind2[j++] = (i + 1) % 36 + 1;
+        ind2[j++] = (i + 1) % 36 + 1 + 38;
+    }
+    //punta giu
+    for (i = 0; i < 36; i++) {
+        ind2[j++] = i + 1;
+        ind2[j++] = (i + 1) % 36 + 1 + 38;
+        ind2[j++] = i + 1 + 38;
+    }
+
+    var color2 = [0.0, 0.0, 1.0];
+    addMesh(vert2, ind2, color2);
 
 
+    // Draws a Sphere --- Already done, just for inspiration
+    var vert3 = [[0.0, 1.0, 0.0]];
+    ///// Creates vertices
+    let k = 1;
+    for (j = 1; j < 18; j++) {
+        for (i = 0; i < 36; i++) {
+            let x = Math.sin(i * 10.0 / 180.0 * Math.PI) * Math.sin(j * 10.0 / 180.0 * Math.PI);
+            let y = Math.cos(j * 10.0 / 180.0 * Math.PI);
+            let z = Math.cos(i * 10.0 / 180.0 * Math.PI) * Math.sin(j * 10.0 / 180.0 * Math.PI);
+            vert3[k++] = [x, y, z];
+        }
+    }
+    vert3[k++] = [0.0, -1.0, 0.0];
+
+    ////// Creates indices
+    var ind3 = [];
+    k = 0;
+    ///////// Lateral part
+    for (i = 0; i < 36; i++) {
+        for (j = 1; j < 17; j++) {
+            ind3[k++] = i + (j - 1) * 36 + 1;
+            ind3[k++] = i + j * 36 + 1;
+            ind3[k++] = (i + 1) % 36 + (j - 1) * 36 + 1;
+
+            ind3[k++] = (i + 1) % 36 + (j - 1) * 36 + 1;
+            ind3[k++] = i + j * 36 + 1;
+            ind3[k++] = (i + 1) % 36 + j * 36 + 1;
+        }
+    }
+    //////// Upper Cap
+    for (i = 0; i < 36; i++) {
+        ind3[k++] = 0;
+        ind3[k++] = i + 1;
+        ind3[k++] = (i + 1) % 36 + 1;
+    }
+    //////// Lower Cap
+    for (i = 0; i < 36; i++) {
+        ind3[k++] = 577;
+        ind3[k++] = (i + 1) % 36 + 540;
+        ind3[k++] = i + 540;
+    }
+
+    var color3 = [0.0, 1.0, 0.0];
+    addMesh(vert3, ind3, color3);
 
 
-	// Draws a Cylinder -- To do for the assignment.
-	var vert2 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,1.0,0.0]];
-	var ind2 = [0, 1, 2,   1, 3, 2];
-	var color2 = [0.0, 0.0, 1.0];
-	addMesh(vert2, ind2, color2);
+    /*
+    x = (R + r * cos(v)) * cos(u);
+    y = (R + r * cos(v)) * sin(u);
+    z = r * sin(v);
+    R = from O to
+     */
+    // Draws a Torus -- To do for the assignment
+    var vert4 = [];
+    let R = 1;
+    let r = 0.4;
+    let arc = Math.PI * 2;
+    k = 0;
+    for (j = 0; j <= 36; j++) {
+        for (i = 0; i <= 27; i++) {
+            var u = i / 27 * arc;
+            var v = j / 36 * arc;
+            let x = (R + r * Math.cos(v)) * Math.cos(u);
+            let y = (R + r * Math.cos(v)) * Math.sin(u);
+            let z = r * Math.sin(v);
+            vert4[k] = [x, y, z];
+            k++;
+        }
 
+    }
+    k = 0;
+	var ind4 = [];
+    for (j = 1; j <= 36; j++) {
+        for (i = 1; i <= 27; i++) {
 
+            var a = (27 + 1) * j + i - 1;
+            var b = (27 + 1) * (j - 1) + i - 1;
+            var c = (27 + 1) * (j - 1) + i;
+            var d = (27 + 1) * j + i;
 
+			ind4.push(a,b,d);
+            ind4.push(b, c, d);
+        }
 
-
-
-	// Draws a Sphere --- Already done, just for inspiration
-	var vert3 = [[0.0, 1.0,0.0]];
-	///// Creates vertices
-	k = 1;
-	for(j = 1; j < 18; j++) {
-		for(i = 0; i < 36; i++) {
-			x = Math.sin(i*10.0/180.0*Math.PI) * Math.sin(j*10.0/180.0*Math.PI);
-			y = Math.cos(j*10.0/180.0*Math.PI);
-			z = Math.cos(i*10.0/180.0*Math.PI) * Math.sin(j*10.0/180.0*Math.PI);
-			vert3[k++] = [x, y, z];
-		}
-	}
-	vert3[k++] = [0.0,-1.0,0.0];
-	
-	////// Creates indices
-	var ind3 = [];
-	k = 0;
-	///////// Lateral part
-	for(i = 0; i < 36; i++) {
-		for(j = 1; j < 17; j++) {
-			ind3[k++] = i + (j-1) * 36 + 1;
-			ind3[k++] = i + j * 36 + 1;
-			ind3[k++] = (i + 1) % 36 + (j-1) * 36 + 1;
-
-			ind3[k++] = (i + 1) % 36 + (j-1) * 36 + 1;
-			ind3[k++] = i + j * 36 + 1;
-			ind3[k++] = (i + 1) % 36 + j * 36 + 1;
-		}
-	}	
-	//////// Upper Cap
-	for(i = 0; i < 36; i++) {
-		ind3[k++] = 0;
-		ind3[k++] = i + 1;
-		ind3[k++] = (i + 1) % 36 + 1;
-	}
-	//////// Lower Cap
-	for(i = 0; i < 36; i++) {
-		ind3[k++] = 577;
-		ind3[k++] = (i + 1) % 36 + 540;
-		ind3[k++] = i + 540;
-	}
-	
-	var color3 = [0.0, 1.0, 0.0];
-	addMesh(vert3, ind3, color3);
-	
-	
-	
-	
-	
-	// Draws a Torus -- To do for the assignment
-	var vert4 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,1.0,0.0]];
-	var ind4 = [0, 1, 2,   1, 3, 2];
-	var color4 = [1.0, 1.0, 0.0];
-	addMesh(vert4, ind4, color4);
+    }
+    // Draws a Torus -- To do for the assignment
+    //var vert4 = [[-1.0,-1.0,0.0], [1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,1.0,0.0]];
+    //var ind4 = [0, 1, 2,   1, 3, 2];
+    var color4 = [1.0, 1.0, 0.0];
+    addMesh(vert4, ind4, color4);
 }
 
