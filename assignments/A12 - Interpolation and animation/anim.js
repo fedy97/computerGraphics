8ea0f32d51a66a845c2154737138606a0d53bc00
fedy97 <frc.morreale@gmail.com>
2020-06-29 16:57:33 +0200

    12 assignment done, small bug to fix

--- assignments/A12 - Interpolation and animation/anim.js
+++ assignments/A12 - Interpolation and animation/anim.js
@@ -1,29 +1,40 @@
 function anim() {
-	return [
- [2, [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0)),
- 	 [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0)),
- 	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
- 	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90))],
- [2, [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
-	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
- 	 [8,0,-8],  Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
- 	 [8,0,-8],  Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90))],
- [2, [8,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
-	 [8,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
- 	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
- 	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180))],
- [2, [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
-	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
- 	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
- 	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270))],
- [2, [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
-	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
- 	 [0,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
- 	 [0,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90))],
- [2, [0,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
-	 [0,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
- 	 [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0)),
- 	 [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0))]
-];
-}
+    let quat0 = Quaternion.fromAxisAngle([1, 0, 0], utils.degToRad(0));
+    let quat1 = Quaternion.fromAxisAngle([0, 1, 0], utils.degToRad(90));
+    let quat2 = Quaternion.fromAxisAngle([0, 1, 0], utils.degToRad(180));
+    quat2 = quat2.mul(Quaternion.fromAxisAngle([0, 0, 1], utils.degToRad(90)));
+    let quat3 = Quaternion.fromAxisAngle([0, 1, 0], utils.degToRad(180));
+    let quat4 = Quaternion.fromAxisAngle([0, 1, 0], utils.degToRad(270));
+    let quat5 = Quaternion.fromAxisAngle([0, 0, 1], utils.degToRad(-90));
+    return [
+        [2, [4, 0, 0], quat0,
+            [8, 0, 0], quat0,
+            [8, 0, 0], quat1,
+            [8, 0, -4], quat1],
 
+        [2, [8, 0, -4], quat1,
+            [8, 0, -8], quat1,
+            [8, 0, -8], quat2,
+            [8, 4, -8], quat2],
+
+        [2, [8, 4, -8], quat2,
+            [8, 8, -8], quat2,
+            [8, 8, -8], quat3,
+            [4, 8, -8], quat3],
+
+        [2, [4, 8, -8], quat3,
+            [0, 8, -8], quat3,
+            [0, 8, -8], quat4,
+            [0, 8, -4], quat4],
+
+        [2, [0, 8, -4], quat4,
+            [0, 8, 0], quat4,
+            [0, 8, 0], quat5,
+            [0, 4, 0], quat5],
+
+        [2, [0, 4, 0], quat5,
+            [0, 0, 0], quat5,
+            [0, 0, 0], quat0,
+            [4, 0, 0], quat0],
+    ];
+}
